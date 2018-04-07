c48784aad926c4c04a03143f913298ce1d784654
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-07 14:52:22 -0700

    Add basic template for index page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'OK'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
