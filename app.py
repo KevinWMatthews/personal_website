c74cc2898572965ff57aa5dffeca48a60b597e54
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-07 15:30:50 -0700

    Add page for embedded systems

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/embedded_code')
+def embedded_code():
+    return render_template('embedded_code.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
