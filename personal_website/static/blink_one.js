c8c39180e56da077a1b8f3ee39ff5219baba4bff
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-22 10:29:25 -0700

    Link button presses to javascript events

--- personal_website/static/blink_one.js
+++ personal_website/static/blink_one.js
@@ -1,3 +1,9 @@
 $(function() {
-    console.log('onload');
+    $('#btn_run-tests_blink-one').click(function() {
+        console.log('Run tests');
+    });
+
+    $('#btn_run-production_blink-one').click(function() {
+        console.log('Run production');
+    });
 });
