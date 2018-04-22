cae38bbe2558e235b81936569d0d0fb7355dea2d
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-22 10:42:49 -0700

    Run blink-one test code

--- personal_website/static/blink_one.js
+++ personal_website/static/blink_one.js
@@ -1,6 +1,12 @@
 $(function() {
     $('#btn_run-tests_blink-one').click(function() {
-        console.log('Run tests');
+        console.log('Blink one, run tests');
+        var jqxhr = $.post( 'blink-one/run-tests', function(data) {
+            console.log('Success: ' + data);
+        })
+        .fail(function(rsp) {
+            console.log('Fail: ' + rsp.status + ': ' + rsp.statusText);
+        });
     });
 
     $('#btn_run-production_blink-one').click(function() {
--- personal_website/views/blink_one.py
+++ personal_website/views/blink_one.py
@@ -1,8 +1,11 @@
 from personal_website import app
-from flask import render_template
+from flask import render_template, jsonify
 
 @app.route('/blink-one', methods=['GET'])
 def blink_one():
     return render_template('blink-one.html')
 
-
+@app.route('/blink-one/run-tests', methods=['POST'])
+def run_tests():
+    print('Blink one: run tests')
+    return jsonify('Python works')
