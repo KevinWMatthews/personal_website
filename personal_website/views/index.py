587d140a524973ac9f636372e9a745c6bc9f662a
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-28 16:34:21 -0700

    Add template and route for C pages

--- personal_website/views/index.py
+++ personal_website/views/index.py
@@ -5,9 +5,15 @@ from flask import render_template, request, redirect, url_for
 def index():
     if request.method == 'GET':
         return render_template('index.html')
-    if 'btnCss' in request.form.keys():
+    if 'btnC' in request.form.keys():
+        return redirect(url_for('c_lang'))
+    elif 'btnCss' in request.form.keys():
         return redirect(url_for('css3'))
     return redirect(url_for('index'))
+
+@app.route('/c-lang', methods=['GET'])
+def c_lang():
+    return render_template('c-lang.html')
 
 @app.route('/css3', methods=['GET'])
 def css3():
