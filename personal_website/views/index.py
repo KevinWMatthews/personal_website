bb5bdc20656b5fa5b4ad68fdf11ba8b7bfbc0587
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-28 16:24:23 -0700

    Add route and html for css3 landing page

--- personal_website/views/index.py
+++ personal_website/views/index.py
@@ -5,4 +5,10 @@ from flask import render_template, request, redirect, url_for
 def index():
     if request.method == 'GET':
         return render_template('index.html')
+    if 'btnCss' in request.form.keys():
+        return redirect(url_for('css3'))
     return redirect(url_for('index'))
+
+@app.route('/css3', methods=['GET'])
+def css3():
+    return render_template('css3.html')
