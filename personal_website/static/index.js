1a17b7c66ae2e4245eab26fe0e3ad187df98d5ef
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-31 09:23:41 -0700

    Add active class to nav links
    Apparently you can't set the underline color?
    I think it looks ok.

--- personal_website/static/index.js
+++ personal_website/static/index.js
@@ -1,9 +1,16 @@
 let navLinks = document.getElementsByClassName('nav-link');
 for (link of navLinks) {
   link.addEventListener('click', function(ev) {
+    // Scroll manually to avoid changing the page's URL
     ev.preventDefault();
     let hash = this.hash;
     let dest = hash.split('#')[1];
     document.getElementById(dest).scrollIntoView();
+
+    // Set class for "active" link for css effect
+    for (link of navLinks) {
+      link.classList.remove('active');
+    }
+    this.classList.add('active');
   });
 }
