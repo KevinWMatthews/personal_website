fad018b26f747a593d595b87e6cc1ea52183ff25
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-05 08:05:22 -0800

    Remove unneeded imports

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -1,5 +1,5 @@
 from personal_website import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template
 
 @app.route('/blog/<post>', methods=['GET'])
 def blog(post):
