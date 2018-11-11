62f5ca99e651764ea7126a75fc0f24a9de86c2c8
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-11 15:47:17 -0800

    Refactor: Change blog post URL

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -2,5 +2,5 @@ from personal_website import app
 from flask import render_template
 
 @app.route('/blog/<post>', methods=['GET'])
-def blog(post):
+def blog_post(post):
     return render_template('{}.html'.format(post))
