7e0a02a4c0a291ce956f1018e67cf2657ebe49b8
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-11 13:23:45 -0800

    Add link back to landing page

--- personal_website/views/index.py
+++ personal_website/views/index.py
@@ -1,7 +1,8 @@
 from personal_website import app
 from flask import render_template, request, redirect, url_for
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
