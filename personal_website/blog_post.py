3ac65a9f48a46a7fbda725f073a18a0ad81237bf
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-11 17:30:31 -0800

    Test blog post title

--- personal_website/blog_post.py
+++ personal_website/blog_post.py
@@ -1,4 +1,5 @@
 class BlogPost():
-    def __init__(self, url, short_text):
+    def __init__(self, url, title, short_text):
         self.url = url
+        self.title = title
         self.short_text = short_text
--- personal_website/tests/test_blog_post.py
+++ personal_website/tests/test_blog_post.py
@@ -4,13 +4,20 @@ from personal_website import blog_post
 class TestBlogPost(unittest.TestCase):
     def setUp(self):
         self.url = 'test-post'
+        self.title = 'Post Title'
         self.short_text = 'Quick intro to blog post'
-        self.post = blog_post.BlogPost(self.url, self.short_text)
+        self.post = blog_post.BlogPost(self.url, self.title, self.short_text)
 
     def test_get_post_url(self):
         '''Get URL of Blog Post'''
         expected = self.url
         actual = self.post.url
+        self.assertEqual(expected, actual)
+
+    def test_get_post_title(self):
+        '''Get title of Blog Post'''
+        expected = self.title
+        actual = self.post.title
         self.assertEqual(expected, actual)
 
     def test_get_post_short_text(self):
