ae84b6ccff55219bca329a7d99d38b6a5f41c7a1
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-11 16:13:49 -0800

    Add landing page for blog, with styling

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -1,6 +1,10 @@
 from personal_website import app
 from flask import render_template
 
+@app.route('/blog', methods=['GET'])
+def blog_home():
+    return render_template('blog_home.html')
+
 @app.route('/blog/<post>', methods=['GET'])
 def blog_post(post):
     return render_template('{}.html'.format(post))
