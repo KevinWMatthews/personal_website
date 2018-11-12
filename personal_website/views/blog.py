c339131cb0a8619e39bb8af4ccea13cf3957d11c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-11 17:46:33 -0800

    Render blog post links from Python

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -2,7 +2,46 @@ from personal_website import app, blog_post
 from flask import render_template
 
 all_blog_posts = [
-    blog_post.BlogPost('c-hello-world', 'Hello, World!', 'Introduction to this blog.')
+    blog_post.BlogPost(
+        'c-hello-world',
+        'Hello, World!',
+        'Introduction to this blog.',
+        ),
+    blog_post.BlogPost(
+        '',
+        'Test-Driven Design on AVR',
+        'Work in progress... based on <a href="https://github.com/KevinWMatthews/c-libATtiny861">this GitHub repo</a>.'
+        ),
+    blog_post.BlogPost(
+        '',
+        'Exit Callbacks using stdlib',
+        'Construct from this skeleton <a href="https://github.com/KevinWMatthews/c-terminate_program">this GitHub repo</a>.',
+        ),
+    blog_post.BlogPost(
+        '',
+        'TLS Using OpenSS',
+        'Experimental but functional. Based on <a href="https://github.com/KevinWMatthews/c-tls_demo">this GitHub repo</a>.',
+        ),
+    blog_post.BlogPost(
+        '',
+        'Varargs (variadics)',
+        'Closest thing to the splat operator that C can provide. Based on <a href="https://github.com/KevinWMatthews/c-varargs_demo">this GitHub repo</a>.',
+        ),
+    blog_post.BlogPost(
+        '',
+        'Parsing Command Line Options',
+        'Better than strcmp() on argv. Based on <a href="https://github.com/KevinWMatthews/c-getopt_demo">this GitHub repo</a>.',
+        ),
+    blog_post.BlogPost(
+        '',
+        'Simple C Socket Server',
+        'And a client, too. Based on <a href="https://github.com/KevinWMatthews/c-server_demo">this GitHub repo</a>.',
+        ),
+    blog_post.BlogPost(
+        '',
+        'Memory Semantics',
+        'Dive under the hood. Based on <a href="https://youtu.be/-dc5vqt2tgA">this awesome video</a>.',
+        ),
 ]
 
 @app.route('/blog', methods=['GET'])
