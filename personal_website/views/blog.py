9bacae4cea0f7de2c194204d030fa7bd298ac875
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-11 17:32:36 -0800

    Render a single blog post entry from an object

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -1,9 +1,13 @@
-from personal_website import app
+from personal_website import app, blog_post
 from flask import render_template
+
+all_blog_posts = [
+    blog_post.BlogPost('c-hello-world', 'Hello, World!', 'Introduction to this blog.')
+]
 
 @app.route('/blog', methods=['GET'])
 def blog_home():
-    return render_template('blog_home.html')
+    return render_template('blog_home.html', posts=all_blog_posts)
 
 @app.route('/blog/<post>', methods=['GET'])
 def blog_post(post):
