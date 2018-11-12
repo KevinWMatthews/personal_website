f370ff35039cacda71966819ecc48db6a1ac6fc8
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-12 06:49:44 -0800

    Add styling to links in blog page

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -10,37 +10,37 @@ all_blog_posts = [
     blog_post.BlogPost(
         '',
         'Test-Driven Design on AVR',
-        'Work in progress... based on <a href="https://github.com/KevinWMatthews/c-libATtiny861">this GitHub repo</a>.'
+        'Work in progress... based on <a href="https://github.com/KevinWMatthews/c-libATtiny861" class="in-text-link">this GitHub repo</a>.'
         ),
     blog_post.BlogPost(
         '',
         'Exit Callbacks using stdlib',
-        'Construct from this skeleton <a href="https://github.com/KevinWMatthews/c-terminate_program">this GitHub repo</a>.',
+        'Construct from this skeleton <a href="https://github.com/KevinWMatthews/c-terminate_program" class="in-text-link">this GitHub repo</a>.',
         ),
     blog_post.BlogPost(
         '',
         'TLS Using OpenSS',
-        'Experimental but functional. Based on <a href="https://github.com/KevinWMatthews/c-tls_demo">this GitHub repo</a>.',
+        'Experimental but functional. Based on <a href="https://github.com/KevinWMatthews/c-tls_demo" class="in-text-link">this GitHub repo</a>.',
         ),
     blog_post.BlogPost(
         '',
         'Varargs (variadics)',
-        'Closest thing to the splat operator that C can provide. Based on <a href="https://github.com/KevinWMatthews/c-varargs_demo">this GitHub repo</a>.',
+        'Closest thing to the splat operator that C can provide. Based on <a href="https://github.com/KevinWMatthews/c-varargs_demo" class="in-text-link">this GitHub repo</a>.',
         ),
     blog_post.BlogPost(
         '',
         'Parsing Command Line Options',
-        'Better than strcmp() on argv. Based on <a href="https://github.com/KevinWMatthews/c-getopt_demo">this GitHub repo</a>.',
+        'Better than strcmp() on argv. Based on <a href="https://github.com/KevinWMatthews/c-getopt_demo" class="in-text-link">this GitHub repo</a>.',
         ),
     blog_post.BlogPost(
         '',
         'Simple C Socket Server',
-        'And a client, too. Based on <a href="https://github.com/KevinWMatthews/c-server_demo">this GitHub repo</a>.',
+        'And a client, too. Based on <a href="https://github.com/KevinWMatthews/c-server_demo" class="in-text-link">this GitHub repo</a>.',
         ),
     blog_post.BlogPost(
         '',
         'Memory Semantics',
-        'Dive under the hood. Based on <a href="https://youtu.be/-dc5vqt2tgA">this awesome video</a>.',
+        'Dive under the hood. Based on <a href="https://youtu.be/-dc5vqt2tgA" class="in-text-link">this awesome video</a>.',
         ),
 ]
 
