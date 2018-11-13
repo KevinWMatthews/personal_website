996c72b43ef03403d5471082fc3b16f0fe3790f6
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-13 08:22:38 -0800

    Add tag list to BlogPost object

--- personal_website/blog_post.py
+++ personal_website/blog_post.py
@@ -1,5 +1,6 @@
 class BlogPost():
-    def __init__(self, url, title, short_text):
+    def __init__(self, url, title, short_text, tags=[]):
         self.url = url
         self.title = title
         self.short_text = short_text
+        self.tags = tags
--- personal_website/tests/test_blog_post.py
+++ personal_website/tests/test_blog_post.py
@@ -6,7 +6,8 @@ class TestBlogPost(unittest.TestCase):
         self.url = 'test-post'
         self.title = 'Post Title'
         self.short_text = 'Quick intro to blog post'
-        self.post = blog_post.BlogPost(self.url, self.title, self.short_text)
+        self.tags = ['Tag1', 'Tag2']
+        self.post = blog_post.BlogPost(self.url, self.title, self.short_text, self.tags)
 
     def test_get_post_url(self):
         '''Get URL of Blog Post'''
@@ -24,4 +25,17 @@ class TestBlogPost(unittest.TestCase):
         '''Get short text of Blog Post'''
         expected = self.short_text
         actual = self.post.short_text
+        self.assertEqual(expected, actual)
+
+    def test_no_tags_by_default(self):
+        '''Blog Post has no tags by default'''
+        post = blog_post.BlogPost(self.url, self.title, self.short_text)
+        expected = []
+        actual = post.tags
+        self.assertEqual(expected, actual)
+
+    def test_get_tag(self):
+        '''Get list of tags from Blog Post'''
+        expected = self.tags
+        actual = self.post.tags
         self.assertEqual(expected, actual)
