7c5810d00d097e1a897576970d11fc2c73b76eef
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-13 08:37:55 -0800

    Add tags to existing posts

--- personal_website/views/blog.py
+++ personal_website/views/blog.py
@@ -6,41 +6,49 @@ all_blog_posts = [
         'hello-world',
         'Hello, World!',
         'Introduction to this blog.',
+        ['C'],
         ),
     blog_post.BlogPost(
         'c-exit-callbacks',
         'Exit Callbacks using stdlib',
         'Construct from this skeleton <a href="https://github.com/KevinWMatthews/c-terminate_program" class="in-text-link">this GitHub repo</a>.',
+        ['C', 'stdlib'],
         ),
     blog_post.BlogPost(
         '',
         'Test-Driven Design on AVR',
-        'Work in progress... based on <a href="https://github.com/KevinWMatthews/c-libATtiny861" class="in-text-link">this GitHub repo</a>.'
+        'Work in progress... based on <a href="https://github.com/KevinWMatthews/c-libATtiny861" class="in-text-link">this GitHub repo</a>.',
+        ['C', 'TDD', 'AVR'],
         ),
     blog_post.BlogPost(
         '',
         'TLS Using OpenSS',
         'Experimental but functional. Based on <a href="https://github.com/KevinWMatthews/c-tls_demo" class="in-text-link">this GitHub repo</a>.',
+        ['C', 'Crypto'],
         ),
     blog_post.BlogPost(
         '',
         'Varargs (variadics)',
         'Closest thing to the splat operator that C can provide. Based on <a href="https://github.com/KevinWMatthews/c-varargs_demo" class="in-text-link">this GitHub repo</a>.',
+        ['C'],
         ),
     blog_post.BlogPost(
         '',
         'Parsing Command Line Options',
         'Better than strcmp() on argv. Based on <a href="https://github.com/KevinWMatthews/c-getopt_demo" class="in-text-link">this GitHub repo</a>.',
+        ['C'],
         ),
     blog_post.BlogPost(
         '',
         'Simple C Socket Server',
         'And a client, too. Based on <a href="https://github.com/KevinWMatthews/c-server_demo" class="in-text-link">this GitHub repo</a>.',
+        ['C'],
         ),
     blog_post.BlogPost(
         '',
         'Memory Semantics',
         'Dive under the hood. Based on <a href="https://youtu.be/-dc5vqt2tgA" class="in-text-link">this awesome video</a>.',
+        ['C'],
         ),
 ]
 
