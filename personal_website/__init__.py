a823a8053cd344b851afff7ab46861ff3ea32a30
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-17 12:57:32 -0800

    Remove unneeded templates, views, and applications
    We'll run code on the server later.
    For now, generate static content.

--- personal_website/__init__.py
+++ personal_website/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-from flask_socketio import SocketIO
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-socketio = SocketIO(app)
 
 from personal_website.views import index
 from personal_website.views import blog
-from personal_website.views import embedded_code, blink_one, blink_two
--- personal_website/views/blink_two.py
+++
@@ -1,6 +0,0 @@
-from personal_website import app
-from flask import render_template, redirect, url_for
-
-@app.route('/blink-two', methods=['GET'])
-def blink_two():
-    return render_template('blink-two.html')
