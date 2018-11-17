7c2eb00800a8651a70f080d3515d8ebdadcc8a04
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-17 12:14:55 -0800

    Remove dead routes

--- personal_website/views/index.py
+++ personal_website/views/index.py
@@ -7,19 +7,3 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     return redirect(url_for('index'))
-
-@app.route('/blog/c', methods=['GET'])
-def c_blog():
-    return render_template('c_blog.html')
-
-@app.route('/blog/python', methods=['GET'])
-def python_blog():
-    return render_template('python_blog.html')
-
-@app.route('/blog/rust', methods=['GET'])
-def rust_blog():
-    return render_template('rust_blog.html')
-
-@app.route('/blog/css3', methods=['GET'])
-def css3_blog():
-    return render_template('css3_blog.html')
