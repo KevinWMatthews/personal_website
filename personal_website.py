4c4065c5481b35010c7fd90044933bbd2859c77f
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-27 09:09:22 -0800

    Add option for Netlify deploy

--- personal_website.py
+++ personal_website.py
@@ -9,12 +9,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create static website content')
     parser.add_argument('-b', '--build', help="Build static pages", action='store_true')
     parser.add_argument('-t', '--test', help="Serve static pages from test server", action='store_true')
+    parser.add_argument('-d', '--deploy', help="Create Netlify deploy", action='store_true')
     args = parser.parse_args()
 
     if args.build:
         print('Building static files')
     elif args.test:
         print('Serving static files')
+    elif args.deploy:
+        print('Creating Netlify deploy')
     else:
         parser.print_help()
         parser.exit()
@@ -25,3 +28,12 @@ if __name__ == '__main__':
         freezer.freeze()
     elif args.test:
         freezer.run()
+    elif args.deploy:
+        freezer.freeze()
+
+        from pathlib import Path
+        import shutil
+        file = Path('_redirects')
+        source = Path('netlify') / file
+        target = Path('app/build') / file
+        shutil.copy(str(source), str(target))
