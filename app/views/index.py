64227a794544b59611f7b1ff2d62175782e8ffba
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-27 08:58:56 -0800

    Rewrite urls for links to blog

--- app/views/index.py
+++ app/views/index.py
@@ -3,22 +3,22 @@ from flask import render_template, request, redirect, url_for
 
 blog_card_c = {
     'text': 'C',
-    'url': 'https://blog.kevinwmatthews.com/category/c',
+    'url': 'https://blog.kevinwmatthews.com/tags/c',
     'id': 'c',
 }
 blog_card_python = {
     'text': 'Python',
-    'url': 'https://blog.kevinwmatthews.com/category/python',
+    'url': 'https://blog.kevinwmatthews.com/tags/python',
     'id': 'python',
 }
 blog_card_rust = {
     'text': 'Rust',
-    'url': 'https://blog.kevinwmatthews.com/category/rust',
+    'url': 'https://blog.kevinwmatthews.com/tags/rust',
     'id': 'rust',
 }
 blog_card_css = {
     'text': 'CSS',
-    'url': 'https://blog.kevinwmatthews.com/category/css',
+    'url': 'https://blog.kevinwmatthews.com/tags/css',
     'id': 'css',
 }
 cards = [
